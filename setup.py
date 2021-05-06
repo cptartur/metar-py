c2a6881dcfd3baf83b09aea1df95edf58f1e0f03
Artur Michalek <artumi1@gmail.com>
2021-05-06 14:58:26 +0200

    Update version to 1.0.dev2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='metar.py',
-    version='1.0.dev1',
+    version='1.0.dev2',
     description='Library allowing easy importing METAR data from NOAA\'s website.',
     author='Artur Michalek',
     packages=find_packages(),
